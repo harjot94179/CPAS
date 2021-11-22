7bd311a534d2f43203e0c37ce75a65209103f054
harjot94179 <52996114+harjot94179@users.noreply.github.com>
2021-11-22 21:27:00 +0530

    Update settings.py

--- CPAS/settings.py
+++ CPAS/settings.py
@@ -135,9 +135,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static")
-# ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
