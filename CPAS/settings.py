92e19e91b59e96b0dd6836ee9f78745cab7f5117
harjot94179 <52996114+harjot94179@users.noreply.github.com>
2021-11-23 00:00:40 +0530

    Update settings.py

--- CPAS/settings.py
+++ CPAS/settings.py
@@ -125,7 +125,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
